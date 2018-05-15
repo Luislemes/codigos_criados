bc452cbbbf2df3dab4a3120d3c0f7301c8bf192b
Haline <luisguilherme.lemes@gmail.com>
2018-05-14 21:25:01 -0300

    Implementando a Interface IMeioTransporte

---
+++ Aulas/Aula1405_Interfaces/Carro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1405_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int velocidade { get; set; }
+
+        public void Acelecar()
+        {
+            velocidade += 10;
+        }
+
+        public void Desacelerar()
+        {
+            velocidade -= 6;
+        }
+    }
+}
--- Aulas/Aula1405_Interfaces/IMeioTransporte.cs
+++ Aulas/Aula1405_Interfaces/IMeioTransporte.cs
@@ -8,6 +8,8 @@ namespace Aula1405_Interfaces
 {
     interface IMeioTransporte
     {
+        int velocidade { get; set; }
+
         void Acelecar();
 
         void Desacelerar();
